ac1b2d4d55695f187b64e8bddeb087ec6e113292
Austin Flatt <austindflatt@gmail.com>
2021-09-21 11:23:06 -0400

    finished task 1

---
+++ what-can-you-do.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const age = Number(prompt('Please enter your age:'));
+
+if (age < 16) {
+    console.log('You cant drive.');
+}
+else if (age >= 16 && age < 17) {
+    console.log('You can drive but not vote.');
+}
+else if (age >= 18 && age === 24) {
+    console.log('You can vote but not rent a car.');
+}
+else {
+    console.log('You can do pretty much anything.');
+}
